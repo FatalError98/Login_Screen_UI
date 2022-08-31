cc8e0c2451e825347a245fdf96a1df1f0692c136
Ammar Talal <103538665+FatalError98@users.noreply.github.com>
2022-08-31 18:40:21 +0300

    g

--- login_screen/lib/screens/login_screen.dart
+++ login_screen/lib/screens/login_screen.dart
@@ -7,7 +7,7 @@ import './home_screen.dart';
 import './signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
-  LoginScreen({Key? key}) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   static const routeName = '/login_screen';
 
@@ -31,69 +31,80 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Background(
-            child: Form(
-          // set the formKey as a key in the Form
-          key: formKey,
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              // vector image
-              Image.asset(
-                'assets/images/Login.png',
-                fit: BoxFit.cover,
-                height: size.height * 0.4,
-              ),
+          child: Form(
+            // set the formKey as a key in the Form
+            key: formKey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                // vector image
+                Image.asset(
+                  'assets/images/Login.png',
+                  fit: BoxFit.cover,
+                  height: size.height * 0.4,
+                ),
 
-              MyTextFormField(
-                controller: emailController,
-                labelText: 'Email',
-                hintText: 'Enter your Email',
-                prefixIcon: const Icon(Icons.email),
-                keyboardtype: TextInputType.emailAddress,
-                errorMessage: "Please enter a valid email",
-              ),
+                MyTextFormField(
+                  controller: emailController,
+                  labelText: 'Email',
+                  hintText: 'Enter your Email',
+                  prefixIcon: const Icon(Icons.email),
+                  keyboardtype: TextInputType.emailAddress,
+                  errorMessage: "Please enter a valid email",
+                  needValid: true,
+                ),
 
-              const SizedBox(
-                height: 20,
-              ),
+                const SizedBox(
+                  height: 20,
+                ),
 
-              MyTextFormField(
-                controller: passwordController,
-                errorMessage: 'Please enter a valid password',
-                hintText: 'Enter your password',
-                labelText: 'Password',
-                prefixIcon: const Icon(Icons.lock),
-              ),
+                MyTextFormField(
+                  controller: passwordController,
+                  isScure: true,
+                  errorMessage: 'Please enter a valid password',
+                  hintText: 'Enter your password',
+                  labelText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                ),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('I don\'t have an Account!'),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed((context), SignUpScreen.routeName);
-                      },
-                      child: const Text('SignUp'))
-                ],
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              MyButton(
-                  buttonText: 'Login',
-                  onPressed: () {
-                    // check the key state if its valid or not
-                    if (formKey.currentState!.validate()) {}
-                    if (emailController.text == 'test@mail.com' &&
-                        passwordController.text == '12345678') {
-                      Navigator.pushNamed((context), HomeScreen.routeName);
-                    }
-                  })
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('I don\'t have an Account!'),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(
+                              (context), SignUpScreen.routeName);
+                        },
+                        child: const Text('SignUp'))
+                  ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                MyButton(
+                    buttonText: 'Login',
+                    onPressed: () {
+                      // check the key state if its valid or not
+                      // if (formKey.currentState!.validate()) {}
+                      if (emailController.text == 'test@mail.com' &&
+                          passwordController.text == '12345678') {
+                        Navigator.pushNamed((context), HomeScreen.routeName);
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content:
+                              Text('Wrong password or password. Try again'),
+                          behavior: SnackBarBehavior.floating,
+                        ));
+                      }
+                    })
+              ],
+            ),
           ),
-        )),
+        ),
       ),
     );
   }
--- login_screen/lib/screens/signup_screen.dart
+++ login_screen/lib/screens/signup_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:login_screen/screens/login_screen.dart';
 
 import '../widgets/background.dart';
 import '../widgets/button.dart';
@@ -27,88 +27,95 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomInset: false,
       appBar: AppBar(),
       body: Background(
           child: Form(
         key: formKey,
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/SignUp.png',
-              fit: BoxFit.cover,
-              height: size.height * 0.4,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                MyTextFormField(
-                  labelText: 'First Name',
-                  hintText: 'Enter your First name',
-                  prefixIcon: const Icon(Icons.person),
-                  errorMessage: 'Enter a valid first name',
-                  controller: firstNameController,
-                  width: 145,
-                  keyboardtype: TextInputType.name,
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                MyTextFormField(
-                  labelText: 'Last Name',
-                  hintText: 'Enter your last name',
-                  prefixIcon: const Icon(Icons.person),
-                  errorMessage: 'Enter a valid last name',
-                  controller: lastNameController,
-                  width: 145,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            MyTextFormField(
-              labelText: 'Email',
-              hintText: 'Enter your email address',
-              prefixIcon: const Icon(Icons.email),
-              errorMessage: 'Enter a valid email address',
-              controller: emailController,
-              needValid: true,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            MyTextFormField(
-              isScure: true,
-              labelText: 'Password',
-              hintText: 'Enter your password',
-              prefixIcon: const Icon(Icons.lock),
-              errorMessage: 'Enter a valid password',
-              controller: passwordController,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            MyTextFormField(
-              isScure: true,
-              labelText: 'Conform Password',
-              hintText: 'ReEnter your password',
-              prefixIcon: const Icon(Icons.lock),
-              errorMessage: 'Enter a valid Password',
-              controller: conformPasswordController,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('I already have an account!'),
-                TextButton(onPressed: () {}, child: const Text('Login'))
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            MyButton(buttonText: 'Sign Up', onPressed: () {})
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/SignUp.png',
+                fit: BoxFit.cover,
+                height: size.height * 0.4,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MyTextFormField(
+                    labelText: 'First Name',
+                    hintText: 'Enter your First name',
+                    prefixIcon: const Icon(Icons.person),
+                    errorMessage: 'Enter a valid first name',
+                    controller: firstNameController,
+                    width: 145,
+                    keyboardtype: TextInputType.name,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  MyTextFormField(
+                    labelText: 'Last Name',
+                    hintText: 'Enter your last name',
+                    prefixIcon: const Icon(Icons.person),
+                    errorMessage: 'Enter a valid last name',
+                    controller: lastNameController,
+                    width: 145,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTextFormField(
+                labelText: 'Email',
+                hintText: 'Enter your email address',
+                prefixIcon: const Icon(Icons.email),
+                errorMessage: 'Enter a valid email address',
+                controller: emailController,
+                needValid: true,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTextFormField(
+                isScure: true,
+                labelText: 'Password',
+                hintText: 'Enter your password',
+                prefixIcon: const Icon(Icons.lock),
+                errorMessage: 'Enter a valid password',
+                controller: passwordController,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTextFormField(
+                isScure: true,
+                labelText: 'Conform Password',
+                hintText: 'ReEnter your password',
+                prefixIcon: const Icon(Icons.lock),
+                errorMessage: 'Enter a valid Password',
+                controller: conformPasswordController,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('I already have an account!'),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            (context), LoginScreen.routeName);
+                      },
+                      child: const Text('Login'))
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyButton(buttonText: 'Sign Up', onPressed: () {})
+            ],
+          ),
         ),
       )),
     );
